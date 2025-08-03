040272fc5ebafde0de815621388179f3c2c6ee2f
Nguyen Do <khoanguyen.do@outlook.com>
2025-08-03 23:05:01 +0700

    feat: use outbound/direction for plate insert

--- dto/geolocation.go
+++ dto/geolocation.go
@@ -13,13 +13,14 @@ type GeolocationInsert struct {
 	Timestamp   time.Time `json:"timestamp"`
 }
 
-type GeolocationByPlateInsert struct {
-	Degree       float32   `json:"degree"`
-	Latitude     float32   `json:"latitude"`
-	Longitude    float32   `json:"longitude"`
-	Speed        float32   `json:"speed"`
-	LicensePlate string    `json:"licensePlate"`
-	RouteID      int64     `json:"routeID"`
-	VariationID  int64     `json:"variationID"`
-	Timestamp    time.Time `json:"timestamp"`
+type GeolocationByPlateAndBoundInsert struct {
+	Degree       float32 `json:"degree"`
+	Latitude     float32 `json:"latitude"`
+	Longitude    float32 `json:"longitude"`
+	Speed        float32 `json:"speed"`
+	LicensePlate string  `json:"licensePlate"`
+	RouteID      int64   `json:"routeID"`
+	// direction = 0 -> IsOutbound = true
+	IsOutbound bool      `json:"isOutbound"`
+	Timestamp  time.Time `json:"timestamp"`
 }
