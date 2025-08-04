28147fd8ae7d88caf0a0a751f3859c149d14c450
Nguyen Do <khoanguyen.do@outlook.com>
2025-08-04 10:48:17 +0700

    feat: add indexes

--- migrations/1748629026872_initial.sql
+++ migrations/1748629026872_initial.sql
@@ -4,13 +4,7 @@ CREATE TABLE vehicles (
     license_plate varchar(10) UNIQUE NOT NULL
 );
 
-CREATE TABLE routes (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    number varchar(10) UNIQUE NOT NULL,
-    name text NOT NULL,
-    ebms_id bigint UNIQUE NULLS NOT DISTINCT,
-    active boolean NOT NULL DEFAULT TRUE
-);
+CREATE UNIQUE INDEX idx_vehicle_licenseplate ON vehicles USING HASH (license_plate);
 
 CREATE TABLE variations (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
@@ -19,6 +13,20 @@ CREATE TABLE variations (
     is_outbound boolean NOT NULL DEFAULT TRUE,
     route_id bigint NOT NULL REFERENCES routes(id)
 );
+
+CREATE INDEX idx_variation_ebmsid ON variations USING HASH (ebms_id);
+
+CREATE UNIQUE INDEX idx_variation_outbound_routeid ON variations(is_outbound, route_id);
+
+CREATE TABLE routes (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    number varchar(10) UNIQUE NOT NULL,
+    name text NOT NULL,
+    ebms_id bigint UNIQUE NULLS NOT DISTINCT,
+    active boolean NOT NULL DEFAULT TRUE
+);
+
+CREATE INDEX idx_route_ebmsid ON routes USING HASH (ebms_id);
 
 CREATE TABLE geolocations (
     degree real NOT NULL,
@@ -35,9 +43,15 @@ CREATE TABLE geolocations (
     tsdb.orderby = 'timestamp DESC'
 );
 
-CREATE UNIQUE INDEX idx_vehicleid_timestamp ON geolocations(vehicle_id, "timestamp");
+CREATE UNIQUE INDEX idx_geolocation_vehicleid_timestamp ON geolocations(vehicle_id, "timestamp");
 
-CREATE UNIQUE INDEX idx_vehicleid_routeid_timestamp ON geolocations(vehicle_id, route_id, "timestamp");
+CREATE UNIQUE INDEX idx_geolocation_vehicleid_routeid_timestamp ON geolocations(vehicle_id, route_id, "timestamp");
 
 -- +goose Down
+DROP TABLE vehicles;
+
+DROP TABLE variations;
+
+DROP TABLE routes;
+
 DROP TABLE geolocations;
